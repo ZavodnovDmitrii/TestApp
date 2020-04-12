726b1bb79f344047fdd62dd9e909893ead23ef1d
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2020-04-12 23:51:56 +0300

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Дмитрий Заводнов on 4/12/20.
+//  Copyright © 2020 Дмитрий Заводнов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
